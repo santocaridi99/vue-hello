5f662052fca0712679aeda167999adc6b6262678
santocaridi99 <santocaridi99@gmail.com>
2022-01-13 16:05:19 +0100

    Bonus aggiunto un button che chiama il metodo saluta

--- js/script.js
+++ js/script.js
@@ -6,4 +6,9 @@ new Vue({
         url:'https://i.picsum.photos/id/320/300/300.jpg?hmac=Ro-_JH-iriuI8r9qjts__9GxeEuFk3okOh9c3G32eZI',
         nome:''
     },
+    methods: {
+        saluta: function () {
+            alert('Ciao '+this.nome)
+        },
+    }
 })
